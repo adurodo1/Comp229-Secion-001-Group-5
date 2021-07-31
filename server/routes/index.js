daedd34e4e11d076fd538803a46285f02795c426
adurodo1 <adurodo1@my.centennialcollege.ca>
2021-07-30 20:40:12 -0400

    AUTHORIZATION REQUIREMENTS CREATED

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+let indexController = require('../controllers/index');
+const { route } = require('./users');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title: 'NST Survey'});
+  res.render('index', {title: 'NST Survey', displayName: req.user ? req.user.displayName: ''});
 });
 
 /* GET home page. */
@@ -17,6 +20,21 @@ router.get('/survey', function(req, res, next){
 })
 
 */
+
+/* GET Route for displaying the Login page */
+router.get('/login', indexController.displayLoginPage);
+
+/* POST Route for processing the Login page */
+router.post('/login', indexController.processLoginPage);
+
+/* GET Route for displaying the Register page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Route for processing the Register page */
+router.post('/register', indexController.processRegisterPage);
+
+/* GET to perform UserLogout */
+router.get('/logout', indexController.performLogout);
 
 router.get('/surveyDB', function(req, res, next){
   res.render('surveyDB', {title: 'Existing Survey Page'})
--- server/routes/surveys.js
+++ server/routes/surveys.js
@@ -17,10 +17,189 @@ router.get('/', (req, res, next) => {
           survey: surveys
         });
       }
-      console.log("sdfsdfsd");
-      console.log(surveys.surveyOwner);
+      
     });
   
   });
 
+  router.get('/add', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+    
+   
+     let a_survey = survey({
+      "surveyOwner":"",
+      "surveyName":"",
+      "createdDate":null,
+      "modifiedDate":"",
+      "url":""
+  });
+  
+     res.render('surveys/details',{title:'Create A Survey',survey:a_survey});
+  
+  
+  });
+
+  router.post('/add', (req, res, next) => {
+
+    /*****************
+     * ADD CODE HERE *
+     *****************/
+
+    //declare id for item associated with CRUD request
+    /* let a_survey = survey({
+      "surveyName":req.body.surveyName,
+      "startDate":req.body.startDate,
+      "endDate":req.body.endDate,
+      "surveyDescription":req.body.surveyDescription
+    
+  });
+
+*/
+  let formRequest = JSON.parse(JSON.stringify(req.body));
+
+   
+    for (var key in formRequest) {
+     if (formRequest.hasOwnProperty(key)) {
+       item = req.body[key];
+  
+       req.body[key]=item;
+       console.log(key+ " " +item);
+     }
+   }
+req.body.surveyName="Unknown";
+   console.log(JSON.parse(JSON.stringify(req.body)));
+   let a_survey2 = survey( formRequest);
+survey.create(a_survey2,(err)=>
+{
+if(err)
+{
+  console.log(err);
+  res.end(err);
+}
+else
+{
+  res.redirect('/surveys');
+}
+
+}); 
+
+});
+
+
+// GET the Book Details page in order to edit an existing Book
+router.get('/:id', (req, res, next) => {
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  
+  //declare id for item associated with CRUD request
+   let id=req.params.id;
+   survey.findById(id,(err,surveys)=>{
+    if(err)
+    {
+        console.log(err);
+        res.end(err);
+    }
+    else
+    {
+        res.render('surveys/editsurvey',{
+            title:'View and Edit Survey',survey:surveys
+        });
+    }
+   });
+  
+
+});
+
+// POST - process the information passed from the details form and update the document
+router.post('/:id', (req, res, next) => {
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+  
+  //declare id for item associated with CRUD request
+   let id=req.params.id;
+
+
+   let updatedSurvey = survey({
+    "_id":id,
+    "surveyName":req.body.name,
+    "startDate":req.body.startDateField,
+    "endDate":req.body.endDateField,
+    "surveyDescription":req.body.surveyDescription
+   }); 
+
+   let updatedSurvey3 = survey({
+    "_id":id,
+    "surveyName":req.body.name,
+    "startDate":req.body.startDateField,
+    "endDate":req.body.endDateField,
+    "surveyDescription":req.body.surveyDescription
+   }); 
+
+
+ let formRequest = JSON.parse(JSON.stringify(req.body));
+
+ req.body._id=id;
+   for (var key in formRequest) {
+    if (formRequest.hasOwnProperty(key)) {
+      item = req.body[key];
+ 
+      req.body[key]=item;
+      console.log(key+ " " +item);
+    }
+  }
+  console.log(JSON.parse(JSON.stringify(req.body)));
+  let updatedSurvey2 = survey( JSON.parse(JSON.stringify(req.body)));
+   survey.updateOne({_id:id},updatedSurvey2,(err)=>
+   {
+       if(err)
+{
+   console.log(err);
+   res.end(err);
+}
+else
+{
+   res.redirect('/surveys');
+}
+   }
+   
+   );
+
+});
+
+
+// GET - process the delete by user id
+router.get('/delete/:id', (req, res, next) => {
+
+  /*****************
+   * ADD CODE HERE *
+   *****************/
+
+  //declare id for item associated with CRUD request
+   let id=req.params.id;
+   survey.remove({_id:id},(err)=>
+   {
+       if(err)
+       {
+           console.log(err);
+   res.end(err);  
+       }
+       else{
+           
+   res.redirect('/surveys');
+       }
+   }
+
+
+   );
+});
+
+  
   module.exports = router;
